4ab71ceb309bc7e2fccf1915dd2e132ba2dc3454
Adam Bray <adam.bray@yale.edu>
2015-07-09 11:04:11 -0400

    updated solution

--- pyramid.js
+++ pyramid.js
@@ -3,19 +3,25 @@ var numRows = prompt("How many rows do you want your triangle to be?");
 
 var currentRow  = 0;
 var rowString   = "";
+var numSpacesToCenter;
+var numCharacters;
 
 while(currentRow < numRows) {
   // start each row with a blank string
   rowString = "";
 
+  // calculate how many spaces and characters to add
+  numSpacesToCenter = (numRows - currentRow) - 1;
+  numCharacters     = (currentRow * 2) + 1;
+
   // add spaces
-  for(var i=0; i <= (numRows - currentRow); i++){
-    rowString += " ";
+  for(var i = 0; i < numSpacesToCenter; i++) {
+    rowString = rowString + " ";
   }
 
   // add chars
-  for(var i=0; i < ((currentRow * 2) + 1); i++){
-    rowString += char;
+  for(var i = 0; i < numCharacters; i++) {
+    rowString = rowString + char;
   }
 
   // display the row
