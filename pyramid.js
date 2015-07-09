f6a668bbe88a412884c12cb66f00358f8a323159
Adam Bray <adam.bray@yale.edu>
2015-07-09 09:23:42 -0400

    added JS solution for pyramid

--- pyramid.js
+++ pyramid.js
@@ -1,1 +1,25 @@
-console.log("hello")
+var char    = prompt("What character do you want to make a triangle from?");
+var numRows = prompt("How many rows do you want your triangle to be?");
+
+var currentRow  = 0;
+var rowString   = "";
+
+while(currentRow < numRows) {
+  // start each row with a blank string
+  rowString = "";
+
+  // add spaces
+  for(var i=0; i <= (numRows - currentRow); i++){
+    rowString += " ";
+  }
+
+  // add chars
+  for(var i=0; i < ((currentRow * 2) + 1); i++){
+    rowString += char;
+  }
+
+  // display the row
+  console.log(rowString);
+
+  currentRow++;
+}
